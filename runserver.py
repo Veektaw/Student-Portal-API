c8c2fd840a7418a5b982298cc2da2c1d82de53ee
Iyayi Victor <veektaw@gmail.com>
2023-03-21 07:19:57 +0100

    Test change

--- runserver.py
+++ runserver.py
@@ -2,7 +2,7 @@ from api import create_app
 from api.config.config import config_dict
 
 
-app = create_app(config=config_dict['prod'])
+app = create_app(config=config_dict['test'])
 
 if __name__ == "__main__":
     app.run()
